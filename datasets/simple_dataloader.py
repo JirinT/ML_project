4892b24b54b4d1d59568642fb803bf2ef96ee959
jghb37 <jan.bergmann@rwth-aachen.de>
2024-02-25 00:18:45 +0100

    some errors resolved

--- datasets/simple_dataloader.py
+++ datasets/simple_dataloader.py
@@ -9,8 +9,12 @@ from PIL import Image
 
 class SimpleDataLoader():
     def __init__(self, data_path, batch_size, preprocessors=None):
-        self.csv_file_name = "print_log_filtered"
+        self.csv_file_name = "caxton_dataset_filtered.csv"
         self.data_path = data_path
+        if self.csv_file_name in os.listdir(self.data_path):
+            self.data_frame = pd.read_csv(os.path.join(self.data_path, self.csv_file_name))
+        else:
+            self.data_frame = None
         self.batch_size = batch_size
         self.num_samples_cumulated_per_folder = []
         self.num_samples = self.count_samples()
@@ -25,24 +29,20 @@ class SimpleDataLoader():
         return self.num_batches
 
     def count_samples(self):
-        num_samples = 0
-        for folder_name in sorted(os.listdir(self.data_path)):
-            folder_path = os.path.join(self.data_path, folder_name)
-            if os.path.isdir(folder_path):
-                if self.csv_file_name in os.listdir(folder_path):
-                    df = pd.read_csv(os.path.join(folder_path, self.csv_file_name))
-                    num_samples += len(df)
-                    self.num_samples_cumulated_per_folder.append(num_samples)
+        if self.data_frame is not None:
+            num_samples = len(self.data_frame)
+        else:
+            num_samples = 0
 
         return num_samples
 
     def create_label(self, df_item):
 
         num_classes_per_label = [3, 3, 3, 3]
-        label_flow_rate = df_item["flow_rate_class"]
-        label_feed_rate = df_item["feed_rate_class"]
-        label_z_offset = df_item["z_offset_class"]
-        label_hotend = df_item["hotend_class"]
+        label_flow_rate = df_item.loc["flow_rate_class"]
+        label_feed_rate = df_item.loc["feed_rate_class"]
+        label_z_offset = df_item.loc["z_offset_class"]
+        label_hotend = df_item.loc["hotend_class"]
         labels = [label_flow_rate, label_feed_rate, label_z_offset, label_hotend]
 
         label_vector = self.one_hot_encoding_multilabel(labels, num_classes_per_label)
@@ -68,7 +68,7 @@ class SimpleDataLoader():
 
         return np.concatenate(one_hot_vectors)
     
-    def load_image(image_path):
+    def load_image(self, image_path):
         """
         Load image data from file.
         
@@ -85,17 +85,7 @@ class SimpleDataLoader():
 
     def load_data(self, idx):
         # Use binary search to find the folder containing the image with the current index
-        folder_idx = bisect_right(self.num_samples_cumulated_per_folder, idx)
-        if folder_idx > 0:
-            file_idx = idx - self.num_samples_cumulated_per_folder[folder_idx - 1]
-        else:
-            file_idx = idx
-
-        folder_name = sorted(os.listdir(self.data_path))[folder_idx]
-        folder_path = os.path.join(self.data_path, folder_name)
-
-        df = pd.read_csv(os.path.join(folder_path, self.csv_file_name))
-        img_path = df["image_path"][file_idx]
+        img_path = self.data_frame["img_path"][idx]
         img_path = os.path.join("./", img_path)
 
         # Load your data from file
@@ -104,7 +94,7 @@ class SimpleDataLoader():
             for p in self.preprocessors:
                 img = p.preprocess(img)
         data = img
-        label = self.create_label(df[file_idx])
+        label = self.create_label(self.data_frame.iloc[idx])
 
         return data, label
     
@@ -117,4 +107,4 @@ class SimpleDataLoader():
             batch_indices = indices[start_idx:end_idx]
             batch_data = [self.load_data(idx) for idx in batch_indices]
 
-            yield batch_data
+            yield batch_idx, batch_data
--- knn_classifier.py
+++ knn_classifier.py
@@ -5,29 +5,35 @@ import cv2
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+
 from preprocessing.simple_preprocessor import SimplePreprocessor
 from datasets.simple_dataloader import SimpleDataLoader
 
 # demo implementation for knn, enter more details later
-data_path = "path/to/your/dataset"
+
+data_path = "./caxton_dataset"
 batch_size = 32
 simple_preprocessor = SimplePreprocessor(width=32, height=32)
-dataloader = SimpleDataLoader(data_path, batch_size=batch_size, preprocessors=simple_preprocessor)
+dataloader = SimpleDataLoader(data_path, batch_size=batch_size, preprocessors=[simple_preprocessor])
 
 knn = KNeighborsClassifier(n_neighbors=3)
 
+max_batches = 100
 
-for batch_data in dataloader:
+for batch_idx, batch_data in tqdm(enumerate(dataloader), total=max_batches, desc="Training"):
     batch_images = []
     batch_labels = []
     
-    for image_data, label in batch_data:
+    for image_data, label in batch_data[1]:
         batch_images.append(image_data)
         batch_labels.append(label)
     
     batch_images_flat = np.array(batch_images).reshape(len(batch_images), -1)
     batch_labels_flat = np.array(batch_labels)
     
-    knn.partial_fit(batch_images_flat, batch_labels_flat, classes=np.unique(batch_labels_flat))
-
+    knn.fit(batch_images_flat, batch_labels_flat)
+    
+    if batch_idx + 1 >= max_batches:
+        break
 
