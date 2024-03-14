417e55c1bbcf2e547e2eb37408305f2075a79b6d
JiriTomasek <jirka.tomasek@email.cz>
2024-03-14 21:36:06 +0100

    python file containing functions for basic folder work

---
+++ folder_functions.py
@@ -0,0 +1,20 @@
+import os
+import matplotlib.pyplot as plt
+
+def create_folder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def delete_files(path):
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
+def save_images(path, images):
+    i = 0
+    for image in images[1:]:
+        filename = f"sample_{i}.png"
+        filepath = os.path.join(path, filename)
+        plt.imsave(filepath, image, cmap='gray')
+        i+=1
