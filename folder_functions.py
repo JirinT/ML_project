a0cd7e08fbf4ef3fcde67a2a4ffae2416f5ae1ef
JiriTomasek <jirka.tomasek@email.cz>
2024-03-19 21:08:14 +0100

    Clearing directiories added

--- folder_functions.py
+++ folder_functions.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import matplotlib.pyplot as plt
 
 def create_folder(path):
@@ -10,6 +11,15 @@ def delete_files(path):
         file_path = os.path.join(path, file)
         if os.path.isfile(file_path):
             os.remove(file_path)
+
+def delete_all_in_folder(path):
+    if os.path.exists(path):
+        for filename in os.listdir(path):
+            file_path = os.path.join(path, filename)
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
 
 def save_images(path, images):
     for i, image in enumerate(images[1:11]):
