Computation of accuracies fixed, loss fcn inputs changed

train.py
@@ -172,6 +172,28 @@ def apply_regularization(model, loss, lambda_regularization, p=1):
     loss += lambda_regularization * l1
     return loss
 
+def one_hot_encode(tensor):
+    # Find the index of the maximum value
+    max_index = tensor.argmax()
+    # Create a new tensor with zeros and a 1 at the max_index
+    one_hot = torch.zeros_like(tensor)
+    one_hot[max_index] = 1
+    # one_hot = one_hot.numpy()
+
+    return one_hot
+
+def decode_predictions(pred_heads_log_prob):
+    prob_heads = [torch.exp(x) for x in pred_heads_log_prob]  # convert to standard probabilities
+    pred_heads_list = []
+    for j in range(len(prob_heads)):
+        pred_head = torch.zeros((len(prob_heads[j]), 3))
+        for i in range(len(prob_heads[j])):
+            pred_head[i] = one_hot_encode(prob_heads[j][i])
+        pred_heads_list.append(pred_head)
+
+    return pred_heads_list
+        
+
 def train():
     # Train the model
     total_step_train = len(train_loader.dataset)
@@ -186,6 +208,9 @@ def train():
         totalValLoss = 0
         trainCorrect = 0 # trainCorrect is the number of correctly predicted samples in total
         valCorrect = 0 # valCorrect is the number of correctly predicted samples in total
+        trainCorrect_total = 0 # trainCorrect_total is the number of correctly predicted samples for all 4 heads
+        valCorrect_total = 0  # valCorrect_total is the number of correctly predicted samples for all 4 heads
+
         trainCorrect_list = [0, 0, 0, 0] # trainCorrect_list is the number of correctly predicted samples for each head
         valCorrect_list = [0, 0, 0, 0] # valCorrect_list is the number of correctly predicted samples for each head
         heads_train_acc = [0, 0, 0, 0] # heads_train_acc is the accuracy for each head
@@ -196,15 +221,26 @@ def train():
             labels = labels.to(device)
             # Forward pass
             if config["cnn"]["model"]["type"]["multihead"]:
-                x1, x2, x3, x4 = model(images)
-                pred_heads = [x1, x2, x3, x4]
+                x1, x2, x3, x4 = model(images) # x1, x2, x3, x4 are outputs of last linear layer - raw data
+                raw_predictions = [x1, x2, x3, x4] # raw_predictions are outputs of last linear layer, before LogSoftMax
+
+                pred_heads_log_prob = [nn.LogSoftmax(dim=1)(x) for x in raw_predictions] # pred_heads_log_prob are the log probabilities
+                pred_heads = decode_predictions(pred_heads_log_prob) # pred_heads is now a list of 4 tensors of shape (batch_size x 3) and contains [0,1,0] for example
+               
                 # losses for each head:
-                loss_1 = criterion(x1, labels[:,:3])
-                loss_2 = criterion(x2, labels[:,3:6])
-                loss_3 = criterion(x3, labels[:,6:9])
-                loss_4 = criterion(x4, labels[:,9:])
+                if config["cnn"]["training"]["loss_function"] != 1:
+                    loss_1 = criterion(x1, labels[:,:3])
+                    loss_2 = criterion(x2, labels[:,3:6])
+                    loss_3 = criterion(x3, labels[:,6:9])
+                    loss_4 = criterion(x4, labels[:,9:])
+                else:
+                    loss_1 = criterion(x1, labels[:,:3].argmax(1))
+                    loss_2 = criterion(x2, labels[:,3:6].argmax(1))
+                    loss_3 = criterion(x3, labels[:,6:9].argmax(1))
+                    loss_4 = criterion(x4, labels[:,9:].argmax(1))
+
                 losses = torch.stack([loss_1, loss_2, loss_3, loss_4])
-                loss = torch.sum(losses) # total loss - MAYBE ADD WEIGHTS TO EACH LOSS???
+                loss = torch.sum(losses) # WRONG - NEEDS TO BE SUMMED JUST BEFORE BACKBONE
             else:
                 pred = model(images)
                 loss = criterion(pred, labels) # calculate the loss for the current batch
@@ -224,10 +260,12 @@ def train():
             optimizer.step()
 
             totalTrainLoss += loss.item()
+
             if config["cnn"]["model"]["type"]["multihead"]:
                 for i in range(len(trainCorrect_list)):
                     trainCorrect_list[i] += (pred_heads[i].argmax(1) == labels[:,3*i:3*(i+1)].argmax(1)).type(torch.float).sum().item()
 
+                trainCorrect_total += (torch.cat(pred_heads, dim=1) == labels).type(torch.float).sum().item()
             else:
                 trainCorrect += (pred.argmax(1) == labels.argmax(1)).type(
                 torch.float).sum().item()
@@ -245,16 +283,27 @@ def train():
 
                 # Forward pass
                 if config["cnn"]["model"]["type"]["multihead"]:
-                    x1, x2, x3, x4 = model(images)
-                    pred_heads = [x1, x2, x3, x4]
+                    x1, x2, x3, x4 = model(images) # x1, x2, x3, x4 are outputs of last linear layer - raw data
+                    raw_predictions = [x1, x2, x3, x4] # raw_predictions are outputs of last linear layer, before LogSoftMax
+
+                    pred_heads_log_prob = [nn.LogSoftmax(dim=1)(x) for x in raw_predictions] # pred_heads_log_prob are the log probabilities
+                    pred_heads = decode_predictions(pred_heads_log_prob) # pred_heads is now vector of shape (batch_size x 12) and contains [0,0,0,1,0,0,0,1,0,0,0,1] for example
+                    
                     # losses for each head:
-                    loss_1 = criterion(x1, labels[:,:3])
-                    loss_2 = criterion(x2, labels[:,3:6])
-                    loss_3 = criterion(x3, labels[:,6:9])
-                    loss_4 = criterion(x4, labels[:,9:])
-                    losses = torch.stack([loss_1, loss_2, loss_3, loss_4])
-                    loss = torch.sum(losses) # total loss - MAYBE ADD WEIGHTS TO EACH LOSS???
-                    # loss = torch.mean(losses) # total loss - MAYBE ADD WEIGHTS TO EACH LOSS???
+                    if config["cnn"]["training"]["loss_function"] != 1:
+                        loss_1 = criterion(x1, labels[:,:3])
+                        loss_2 = criterion(x2, labels[:,3:6])
+                        loss_3 = criterion(x3, labels[:,6:9])
+                        loss_4 = criterion(x4, labels[:,9:])
+                    else:
+                        loss_1 = criterion(x1, labels[:,:3].argmax(1))
+                        loss_2 = criterion(x2, labels[:,3:6].argmax(1))
+                        loss_3 = criterion(x3, labels[:,6:9].argmax(1))
+                        loss_4 = criterion(x4, labels[:,9:].argmax(1))
+
+                    losses = torch.stack([loss_1, loss_2, loss_3, loss_4]) 
+                    loss = torch.sum(losses) # WRONG!!!! WE NEED TO SUM IT JUST BEFORE BACKBONE
+
                 else:
                     pred = model(images)
                     loss = criterion(pred, labels) # calculate the loss for the current batch
@@ -271,6 +320,7 @@ def train():
                 if config["cnn"]["model"]["type"]["multihead"]:
                     for i in range(len(valCorrect_list)):
                         valCorrect_list[i] += (pred_heads[i].argmax(1) == labels[:,3*i:3*(i+1)].argmax(1)).type(torch.float).sum().item()
+                    valCorrect_total += (torch.cat(pred_heads, dim=1) == labels).type(torch.float).sum().item()
                 else:
                     trainCorrect += (pred.argmax(1) == labels.argmax(1)).type(
                     torch.float).sum().item()
@@ -278,16 +328,14 @@ def train():
         avgTrainLoss = totalTrainLoss / total_step_train
         avgValLoss = totalValLoss / total_step_val
         if config["cnn"]["model"]["type"]["multihead"]:
-            trainCorrect_total = sum(trainCorrect_list) # trainCorrect_total is sum of correctly predicted samples for all 4 heads
-            valCorrect_total = sum(valCorrect_list) # valCorrect_total is sum of correctly predicted samples for all 4 heads
             # train accuracies:
-            train_acc = trainCorrect_total / (total_step_train*4) # train_acc is the overall accuracy predicting all 4 heads
+            train_acc = trainCorrect_total / total_step_train # train_acc is the overall accuracy predicting all 4 heads
             # train accuracies for each head:
             for i in range(len(trainCorrect_list)):
                 heads_train_acc[i] = trainCorrect_list[i] / total_step_train
 
             # val accuracies:
-            val_acc = valCorrect_total / (total_step_val*4) # val_acc is the overall accuracy predicting all 4 heads
+            val_acc = valCorrect_total / total_step_val # val_acc is the overall accuracy predicting all 4 heads
             # val accuracies for each head:
             for i in range(len(valCorrect_list)):
                 heads_val_acc[i] = valCorrect_list[i] / total_step_val