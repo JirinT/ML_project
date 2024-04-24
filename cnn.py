824e0891bdb8c3bc9b508932226274cf4d659d15
JiriTomasek <jirka.tomasek@email.cz>
2024-04-24 19:25:02 +0200

    Batch normalization is just in if cond. now

--- cnn.py
+++ cnn.py
@@ -95,7 +95,6 @@ class CNN4(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=num_channels, out_channels=16, kernel_size=5, stride=1, padding=2)
         if config["cnn"]["training"]["normalization"]["use_batch_normalization"]:
             self.bn1 = nn.BatchNorm2d(16)
-        self.bn1 = nn.BatchNorm2d(16)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
